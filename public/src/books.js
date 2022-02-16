a9a83eff60e41b0ca75a072752719bb84bdbf935
Sydney Workman <sworkman1996@gmail.com>
2022-02-15 19:38:03 -0600

    books functions complete

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,41 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  return authors.find(author => author.id === id);
+}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  return books.find(book => book.id === id);
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  // return array with two nested arrays
+  // array 1: checked out books
+  // array 2: returned books
+  const checkedOut = books.filter((book) => {
+    if (!book.borrows[0].returned) return book;
+  });
+  const returned = books.filter((book) => {
+    if (book.borrows[0].returned) return book;
+  });
+  
+  return [checkedOut, returned];
+}
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  const borrowers = [];
+
+  book.borrows.forEach(borrow => {
+    const { id, returned } = borrow;
+    // find accounts whose id is within borrowed.
+    let account = accounts.find(acc => {
+      return acc.id === id;
+    });
+    // return account info plus return status.
+    borrowers.push({...account, returned});
+  });
+
+  // limit array to 10 members.
+  return borrowers.length > 10 ? borrowers.slice(0,10) : borrowers;
+}
 
 module.exports = {
   findAuthorById,
